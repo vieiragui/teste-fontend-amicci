f91c927cf6c8966ebb4f15145c93a4f6d004caeb
Guilherme Vieira <vieiragui21@gmail.com>
2024-01-30 07:48:33 -0300

    Alterando configuração para publicação

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   /**
    * Enable static exports for the App Router.
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
+  output: 'out', // Especifique o diretório de saída desejado
 
   /**
    * Set base path. This is the slug of your GitHub repository.
