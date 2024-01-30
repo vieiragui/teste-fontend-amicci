15115a8b483274de398da772d89bcbd3a7f29aa3
Guilherme Vieira <vieiragui21@gmail.com>
2024-01-30 07:07:41 -0300

    estilizando resultado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { cityService } from "@/service/city-service";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { weatherService } from "@/service/weather-service";
 import { useEffect, useState } from "react";
 import { Coordinates } from "@/interface/coordinates";
+import { animated, useSpring } from "react-spring";
 
 const schema = z.object({
   city: z.string().trim().min(4, { message: 'Cidade é obrigatório' })
@@ -16,11 +17,17 @@ const schema = z.object({
 type FormProps = z.infer<typeof schema>
 
 export default function Home() {
-  const { register, handleSubmit, formState: { errors, isValid }, resetField } = useForm<FormProps>({
+  const { register, handleSubmit, formState: { isValid }, resetField } = useForm<FormProps>({
     resolver: zodResolver(schema)
   })
   const [weathers, setWeathers] = useState<any>([])
   const [coordinatesCurrent, setCoordinatesCurrent] = useState<Coordinates>()
+  const [isSearching, setIsSearching] = useState(false);
+
+  const fadeIn = useSpring({
+    opacity: isSearching ? 1 : 0,
+    transform: isSearching ? 'translateY(0)' : 'translateY(20px)',
+  });
 
   const submitForm = async (data: any) => {
     const coordinates = await cityService.getCoordinates(data.city);
@@ -42,6 +49,7 @@ export default function Home() {
   }
 
   const currentWeather = async () => {
+    setIsSearching(true)
     const weather = await weatherService.getWeather(coordinatesCurrent as Coordinates);
 
     setWeathers(weather)
@@ -58,8 +66,13 @@ export default function Home() {
         <form className="md:max-w-2xl w-full p-4 bg-white shadow-md rounded-md" onSubmit={handleSubmit(submitForm)}>
           <div className="relative mb-4">
 
-            <TextField {...register('city')} className="shadow-2xl w-full rounded-md" id="outlined-basic" label="Digite sua Cidade" variant="outlined" />
-
+            <TextField
+              {...register('city')}
+              className="shadow-2xl w-full rounded-md"
+              id="outlined-basic"
+              label="Digite sua Cidade"
+              variant="outlined"
+            />
 
             <div className="flex items-center justify-center space-x-5 my-5">
               <Button disabled={!isValid} variant="contained" color="primary" type="submit" className="mr-2">
@@ -71,9 +84,11 @@ export default function Home() {
               </Button>
             </div>
 
-            <div className="mt-4">
-              <p>Temperatura: {weathers}</p>
-            </div>
+            <animated.div style={fadeIn} className="mt-4">
+              <Typography variant="h6" component="div" color="textPrimary">
+                Clima Atual: {weathers}
+              </Typography>
+            </animated.div>
           </div>
         </form>
       </div>
